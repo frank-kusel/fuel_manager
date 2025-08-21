f5105e656b6b8783093944952d5445da292bd1d1
DasFranki <frankuse1@gmail.com>
2025-08-21 11:28:17 +0200

    update toml

--- .netlify/server/nodes/2.js
+++ .netlify/server/nodes/2.js
@@ -3,6 +3,6 @@
 export const index = 2;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.BPxqWggE.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/B-pUM7Bg.js","_app/immutable/chunks/D6cbYCli.js","_app/immutable/chunks/5P4c1mN1.js","_app/immutable/chunks/CMbyeBjM.js","_app/immutable/chunks/BVOnU6Vx.js","_app/immutable/chunks/CSIOR0MX.js"];
+export const imports = ["_app/immutable/nodes/2.D6pbkai3.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/B-pUM7Bg.js","_app/immutable/chunks/D6cbYCli.js","_app/immutable/chunks/5P4c1mN1.js","_app/immutable/chunks/CMbyeBjM.js","_app/immutable/chunks/nXdQvSCr.js","_app/immutable/chunks/CSIOR0MX.js"];
 export const stylesheets = ["_app/immutable/assets/2.BQu_2Vgr.css"];
 export const fonts = [];
